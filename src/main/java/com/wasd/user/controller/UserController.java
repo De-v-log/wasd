414eb58ba211c5dc8c3a3e5a75f724971836c1fa
yjlee <81798918+y-00jin@users.noreply.github.com>
2024-09-23 08:42:58 +0900

    project settings

---
+++ src/main/java/com/wasd/user/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.wasd.user.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/user/login")
+    public ModelAndView login(ModelAndView model) {
+        model.addObject("data", "test");
+        model.setViewName("/user/login");
+        return model;
+    }
+}
