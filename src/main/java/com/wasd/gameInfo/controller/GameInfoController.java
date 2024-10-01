c99cf27c5b1e3286a683b9d3fbd6434cd78479e4
keartt <shalpha_2@naver.com>
2024-10-01 23:32:01 +0900

    [FIX] 유저 게임 정보 가져오기
    - 유저 게임목록, 유저 특정 게임 목록 가져오기 기능 추가
    - URL restful 하게 수정
    - 모든 정보 dto 로 리턴할 수 있도록 수정

--- src/main/java/com/wasd/gameInfo/controller/GameInfoController.java
+++ src/main/java/com/wasd/gameInfo/controller/GameInfoController.java
@@ -1,20 +1,16 @@
 package com.wasd.gameInfo.controller;
 
 import com.wasd.gameInfo.dto.GameInfoDto;
-import com.wasd.gameInfo.entity.GameInfo;
-import com.wasd.gameInfo.entity.UserGameInfo;
 import com.wasd.gameInfo.service.GameInfoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
 @RestController
+@RequestMapping("/gameInfo")
 @RequiredArgsConstructor
 public class GameInfoController {
     private final GameInfoService gameInfoService;
@@ -23,9 +19,9 @@ public class GameInfoController {
      * 전체 게임 목록 반환
      * @return 게임아이디(영문) 게임명(한글) 목록 반환
      */
-    @GetMapping("/gameInfo")
+    @GetMapping
     public ResponseEntity<List<GameInfoDto>> findGameInfo(){
-        return ResponseEntity.ok(gameInfoService.findGameList());
+        return ResponseEntity.ok(gameInfoService.findGameInfo());
 
     }
 
@@ -33,9 +29,20 @@ public class GameInfoController {
      * 사용자가 선택한 게임의 속성값들 반환
      * @param gameId 게임이름
      */
-    @GetMapping("/gameInfo/{gameId}")
-    public ResponseEntity<GameInfo> findGameInfo(@PathVariable String gameId){
-        return ResponseEntity.ok(gameInfoService.findGameInfoByGameId(gameId));
+    @GetMapping("/{gameId}")
+    public ResponseEntity<GameInfoDto> findGameInfo(@PathVariable String gameId){
+        return ResponseEntity.ok(gameInfoService.findGameInfo(gameId));
+    }
+
+
+    /**
+     * 유저의 게임 목록들 반환
+     * @param userId 유저아이디
+     * @return List<GameInfoDto> 게임목록
+     */
+    @GetMapping("/user")
+    public ResponseEntity<List<GameInfoDto>> findUserGameInfo(@RequestParam String userId){
+        return ResponseEntity.ok(gameInfoService.findUserGameInfo(userId));
     }
 
     /**
@@ -44,8 +51,8 @@ public class GameInfoController {
      * @param gameId 게임아이디
      * @return GameInfo 해당 게임정보
      */
-    @GetMapping("/gameInfo/user")
-    public ResponseEntity<GameInfo> findUserGameInfo(@RequestParam String userId, @RequestParam String gameId){
+    @GetMapping("/user/game")
+    public ResponseEntity<GameInfoDto> findUserGameInfo(@RequestParam String userId, @RequestParam String gameId){
         return ResponseEntity.ok(gameInfoService.findUserGameInfo(userId, gameId));
     }
 }
--- src/main/java/com/wasd/gameInfo/repository/UserGameInfoRepository.java
+++ src/main/java/com/wasd/gameInfo/repository/UserGameInfoRepository.java
@@ -7,8 +7,15 @@ import org.springframework.data.mongodb.repository.Query;
 import java.util.Optional;
 
 public interface UserGameInfoRepository extends MongoRepository<UserGameInfo, String> {
-    @Query("{ 'userId': ?0, 'gameInfoList.gameId': ?1 }")
-    UserGameInfo findByUserIdAndGameId(String userId, String gameId);
+    @Query(value = "{ 'userId': ?0, 'gameInfoList': { $elemMatch: { 'gameId': ?1 } } }",
+            fields = "{ 'gameInfoList.$': 1 }")
+    Optional<UserGameInfo> findByUserIdAndGameId(String userId, String gameId);
 
     Optional<UserGameInfo> findByUserId(String userId);
+
+    @Query(value = "{ 'userId': ?0 }",
+            fields = "{ 'gameInfoList.gameId': true, 'gameInfoList.gameNm' :true}")
+    Optional<UserGameInfo> findUserGameInfoListByUserId(String userId);
+
+
 }
